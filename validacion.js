26593f9cd07848f47450f1fc2ce438106118c02e
sergio iribe <sergio.iribec@gmail.com>
2023-11-01 00:01:25 -0700

    portafolio sencillo no responsive

--- validacion.js
+++ validacion.js
@@ -1,1 +1,16 @@
-//Haz tú validación en javascript acá
+const pantalla = document.getElementsByClassName(".hola");
+
+function mostrar() {
+  pantalla.classList.add("active");
+}
+
+function ocultar() {
+  if (pantalla.classList.contains("active")) {
+    pantalla.classList.remove("active");
+  }
+}
+ 
+function send (e) {
+    e.preventdefault()
+    
+}
